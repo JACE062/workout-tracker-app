68d144f43bd818dafd7657331027b53e3bf2617c
Makaden Espinosa <mespinosa@student.neumont.edu>
2026-03-11 12:37:51 -0600

    Removing About Page, Styling the main page

--- WorkoutTrackerApp/MainPage.xaml.cs
+++ WorkoutTrackerApp/MainPage.xaml.cs
@@ -13,19 +13,10 @@ namespace WorkoutTrackerApp
             InitializeComponent();
         }
 
-
-        //private void OnCounterClicked(object? sender, EventArgs e)
-        //{
-        //    count++;
-
-        //    if (count == 1)
-        //        CounterBtn.Text = $"Clicked {count} time";
-        //    else
-        //        CounterBtn.Text = $"Clicked {count} times";
-
-        //    SemanticScreenReader.Announce(CounterBtn.Text);
-        //}
-
+        private async void OnUserButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//UserEntry");
+        }
 
     }
 }
